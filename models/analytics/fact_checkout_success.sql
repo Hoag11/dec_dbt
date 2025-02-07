4d128321b663d1e8d0062c2304be3790ab5740e9
Hoag11 <nghuyhoang1101@gmail.com>
2025-02-08 01:02:27 +0700

    update

--- models/analytics/fact_checkout_success.sql
+++ models/analytics/fact_checkout_success.sql
@@ -3,17 +3,11 @@ SELECT
     ,u.user_id
     ,l.location_id
     ,t.time_id
-    ,m.marketing_id
 FROM {{ source('glamira','dec_raw_data')}} g
 JOIN {{ ref("dim_user")}} u
-    ON g.ip = u.ip 
-    AND g.user_agent = u.user_agent    
+    ON g.device_id = u.device_id 
 JOIN {{ ref("dim_location")}} l
     ON g.ip = l.ip
 JOIN {{ ref("dim_time")}} t
     ON g.local_time = t.local_time
-JOIN {{ ref("dim_marketing")}} m
-    ON g.utm_source = m.utm_source
-    AND g.utm_medium = m.utm_medium
-    AND g.refferer_url = m.referrer_url
 WHERE g.collection = 'checkout_success'
