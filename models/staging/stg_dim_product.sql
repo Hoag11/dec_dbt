9586bd2f860ffb490342b02e3c7d1d22e77256a9
Hoag <nghuyhoang1101@gmail.com>
2025-02-07 14:15:41 +0700

    update

---
+++ models/staging/stg_dim_product.sql
@@ -0,0 +1,5 @@
+SELECT product_id 
+        ,current_url AS url
+        ,COALESCE(product_name, 'undefined') AS product_name
+        ,COALESCE(translated_product_name, 'undefined') AS translated_name
+FROM {{source('glamira','dec_product_raw')}}
---
+++ models/staging/stg_dim_time.sql
@@ -0,0 +1,12 @@
+SELECT 
+    time_stamp AS timestamp,
+    DATE(local_time) AS date,
+    EXTRACT(YEAR FROM (local_time)) AS year,
+    EXTRACT(QUARTER FROM (local_time)) AS quarter,
+    EXTRACT(MONTH FROM (local_time)) AS month,
+    FORMAT_TIMESTAMP('%B', (local_time)) AS month_name,
+    EXTRACT(WEEK FROM (local_time)) AS week_of_year,
+    EXTRACT(DAY FROM (local_time)) AS day,
+    EXTRACT(DAYOFWEEK FROM (local_time)) AS day_of_week,
+    FORMAT_TIMESTAMP('%A', (local_time)) AS day_name,
+FROM {{source('glamira','dec_raw_data')}}
